ca44aab75adf22b51f11983f7299e9124cbe7608
RobertWeidinger <robert@familie.weidinger-rr.de>
2013-10-28 22:55:48 +0100

    Klasse NeighbourIterator angelegt. Test in Klasse NeighbourIteratorTest angelegt (noch rot).

---
+++ src/de/rmrw/ReversiKata/code/NeighbourIterator.java
@@ -0,0 +1,26 @@
+package de.rmrw.ReversiKata.code;
+
+import java.util.Iterator;
+
+public class NeighbourIterator implements Iterator<Pos> {
+
+	public NeighbourIterator(Spielfeld spielfeld, Pos pos) {
+		throw new RuntimeException("noch nicht implementiert");
+	}
+
+	@Override
+	public boolean hasNext() {
+		throw new RuntimeException("noch nicht implementiert");
+	}
+
+	@Override
+	public Pos next() {
+		throw new RuntimeException("noch nicht implementiert");
+	}
+
+	@Override
+	public void remove() {
+		throw new RuntimeException("wird nicht benotigt und ist deshalb nicht implementiert.");
+	}
+
+}
