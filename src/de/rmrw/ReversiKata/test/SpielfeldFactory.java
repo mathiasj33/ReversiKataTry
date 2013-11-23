7ec0c90c43fddb879c068e6ff65e7a6467583470
RobertWeidinger <robert@familie.weidinger-rr.de>
2013-11-23 16:06:00 +0100

    1. SpielfeldTest weiter ge-refactored. Die IteratorStub-Klassen werden nicht mehr benötigt und sind deshalb gelöscht. 2. Methode testSetzeSpielstein implementiert. Sie wartet auf ihren Code...

--- src/de/rmrw/ReversiKata/test/SpielfeldFactory.java
+++ src/de/rmrw/ReversiKata/test/SpielfeldFactory.java
@@ -31,38 +31,85 @@ public class SpielfeldFactory {
 		//o  w  b
 		//o  o  o
 		//o  o  o
-		sp = new Spielfeld(3);
+		sp = getSpielfeld3x3ForEsGibtKeinenWegVonPosZuFarbeHorizontal();
 		sp.setForInit(Colors.WHITE, new Pos(0,1));
-		sp.setForInit(Colors.BLACK, new Pos(0,2));
 		return sp;
 	}
 
+	public static Spielfeld getSpielfeld3x3ForEsGibtKeinenWegVonPosZuFarbeHorizontal() {
+		//o  o  b
+		//o  o  o
+		//o  o  o
+		sp = new Spielfeld(3);
+		sp.setForInit(Colors.BLACK, new Pos(0,2));
+		return sp;
+	}
+	
+	
 	public static Spielfeld getSpielfeld4x4ForEsGibtEinenWegVonPosZuFarbeVertikal() {
 		//b  o  o  o
 		//w  o  o  o
 		//w  o  o  o
 		//o  o  o  o
-		sp = new Spielfeld(4);
-		sp.setForInit(Colors.BLACK, new Pos(0,0));
-		sp.setForInit(Colors.WHITE, new Pos(1,0));
+		sp = getSpielfeld4x4ForEsGibtKeinenWegVonPosZuFarbeVertikal();
 		sp.setForInit(Colors.WHITE, new Pos(2,0));
 		return sp;
 	}
 
+	public static Spielfeld getSpielfeld4x4ForEsGibtKeinenWegVonPosZuFarbeVertikal() {
+		//b  o  o  o
+		//w  o  o  o
+		//o  o  o  o
+		//o  o  o  o
+		sp = new Spielfeld(4);
+		sp.setForInit(Colors.BLACK, new Pos(0,0));
+		sp.setForInit(Colors.WHITE, new Pos(1,0));
+		return sp;
+	}
+
+	
 	public static Spielfeld getSpielfeld4x4ForEsGibtEinenWegVonPosZuFarbeDiagonal() {
 		//o  o  o  o
 		//o  o  w  o
 		//o  w  o  o
 		//b  o  o  o
+		sp =getSpielfeld4x4ForEsGibtKeinenWegVonPosZuFarbeDiagonal();
+		sp.setForInit(Colors.WHITE, new Pos(2,1));
+		return sp;
+	}
+	
+	public static Spielfeld getSpielfeld4x4ForEsGibtKeinenWegVonPosZuFarbeDiagonal() {
+		//o  o  o  o
+		//o  o  w  o
+		//o  o  o  o
+		//b  o  o  o
 		sp = new Spielfeld(4);
 		sp.setForInit(Colors.BLACK, new Pos(3,0));
-		sp.setForInit(Colors.WHITE, new Pos(2,1));
 		sp.setForInit(Colors.WHITE, new Pos(1,2));
 		return sp;
 	}
+
 
 	public static Spielfeld getSpielfeld2x2ForContains() {
 		sp = new Spielfeld(2);
 		return sp;
 	}
+
+	public static Spielfeld getSpielfeld4x4ForSetzeSpielstein() {
+		sp = new Spielfeld(4);
+		// o  w  b  o 
+		// o  o  b  b
+		// o  w  o  b
+		// o  o  o  w
+		sp.setForInit(Colors.WHITE, new Pos(0,1));
+		sp.setForInit(Colors.BLACK, new Pos(0,2));
+		sp.setForInit(Colors.BLACK, new Pos(1,2));
+		sp.setForInit(Colors.BLACK, new Pos(1,3));
+		sp.setForInit(Colors.WHITE, new Pos(2,1));
+		sp.setForInit(Colors.BLACK, new Pos(2,3));
+		sp.setForInit(Colors.WHITE, new Pos(3,3));
+		return sp;
+	}
+
+
 }
--- src/de/rmrw/ReversiKata/test/SpielfeldTest.java
+++ src/de/rmrw/ReversiKata/test/SpielfeldTest.java
@@ -1,7 +1,6 @@
 package de.rmrw.ReversiKata.test;
 
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.*;
 
 import java.util.Set;
 
@@ -9,16 +8,21 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import de.rmrw.ReversiKata.code.Colors;
+import de.rmrw.ReversiKata.code.DirectionIterator;
+import de.rmrw.ReversiKata.code.LineIterator;
 import de.rmrw.ReversiKata.code.Pos;
 import de.rmrw.ReversiKata.code.Spielfeld;
+import de.rmrw.ReversiKata.code.SpielfeldIterator;
 
 public class SpielfeldTest {
 	
-	private Spielfeld spielfeld; 
+	
 	
 	@Test 
 	public final void testGetColor() {
-		spielfeld = SpielfeldFactory.getSpielfeld2x2ForTestGetColorAndForToString();
+		Spielfeld spielfeld = SpielfeldFactory.getSpielfeld2x2ForTestGetColorAndForToString();
+		// b  o
+		// o  w
 		Assert.assertEquals(Colors.BLACK, spielfeld.getColor(new Pos(0,0)));
 		Assert.assertEquals(Colors.VOID, spielfeld.getColor(new Pos(0,1)));
 		Assert.assertEquals(Colors.WHITE, spielfeld.getColor(new Pos(1,1)));
@@ -27,10 +31,15 @@ public class SpielfeldTest {
 
 	@Test
 	public final void testToString() {
-		// Partielles Mocking
+		// mock Iterator
+		SpielfeldIterator mockSpielfeldIterator = mock(SpielfeldIterator.class);
+		when(mockSpielfeldIterator.hasNext()).thenReturn(true, true, true, true, false);
+		when(mockSpielfeldIterator.next()).thenReturn( new Pos(0,0), new Pos(0,1), new Pos(1,0), new Pos(1,1));
+		// mock Iterator - Ende
+		// Partielles Mocking Spielfeld
 		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld2x2ForTestGetColorAndForToString());
-		doReturn(new SpielfeldIteratorStub(spySpielfeld)).when(spySpielfeld).createSpielfeldIterator();
-		// Mock-Ende;
+		doReturn(mockSpielfeldIterator).when(spySpielfeld).createSpielfeldIterator();
+		// Partielles Mocking Spielfeld - Ende
 		String s = spySpielfeld.toString();
 		Assert.assertEquals("b o"+System.getProperty("line.separator")+
 							"o w"+System.getProperty("line.separator"),
@@ -81,37 +90,142 @@ public class SpielfeldTest {
 	
 	@Test
 	public void testContains() {
-		spielfeld = SpielfeldFactory.getSpielfeld2x2ForContains();
+		Spielfeld spielfeld = SpielfeldFactory.getSpielfeld2x2ForContains();
 		Assert.assertTrue(spielfeld.contains(new Pos(1,1)));
 		Assert.assertFalse(spielfeld.contains(new Pos(2,0)));
 	}
 
 	@Test
 	public void testEsGibtEinenWegVonPosZuFarbeHorizontal() {  
-		// partielles Mocking - DirectionIterator und LineIterator werden gemockt
 		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld3x3ForEsGibtEinenWegVonPosZuFarbeHorizontal());
-		doReturn(new DirectionIteratorStubReturns0_1and1_1()).when(spySpielfeld).createDirectionIterator();
-		doReturn(new LineIteratorStub3x3Directions0_1and1_1(spySpielfeld,new Pos(0,0), new Pos(0,1))).when(spySpielfeld).createLineIterator(new Pos(0,0), new Pos(0,1));
-		doReturn(new LineIteratorStub3x3Directions0_1and1_1(spySpielfeld,new Pos(0,0), new Pos(1,1))).when(spySpielfeld).createLineIterator(new Pos(0,0), new Pos(1,1));
-		// Mocking-Ende
-		
+		//o  w  b
+		//o  o  o
+		//o  o  o
+		mockingFuerEsGibtEinenOderKeinenHorizontal(spySpielfeld);
 		Assert.assertTrue(spySpielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(0,0), Colors.BLACK));
+	}
+
+	@Test
+	public void testEsGibtKeinenWegVonPosZuFarbeHorizontal() {  
+		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld3x3ForEsGibtKeinenWegVonPosZuFarbeHorizontal());
+		//o  o  b
+		//o  o  o
+		//o  o  o
+		mockingFuerEsGibtEinenOderKeinenHorizontal(spySpielfeld);
 		Assert.assertFalse(spySpielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(1,1), Colors.BLACK));
+	}
+
+
+	private void mockingFuerEsGibtEinenOderKeinenHorizontal(
+			Spielfeld spySpielfeld) {
+		// Mock Iteratoren
+		DirectionIterator mockDI = mock(DirectionIterator.class);
+		when(mockDI.hasNext()).thenReturn( true, true, false);
+		when(mockDI.next()).thenReturn( new Pos(0,1), new Pos(1,1));
+		
+		LineIterator mockLIHorizontal = mock(LineIterator.class);
+		when(mockLIHorizontal.hasNext()).thenReturn( true, true, true, false);
+		when(mockLIHorizontal.next()).thenReturn( new Pos(0,0),new Pos(0,1),new Pos(0,2));
+		
+		LineIterator mockLIDiagonal = mock(LineIterator.class);
+		when(mockLIDiagonal.hasNext()).thenReturn( true, true, true, false);
+		when(mockLIDiagonal.next()).thenReturn( new Pos(0,0),new Pos(1,1),new Pos(2,2));
+		//  Mock Iteratoren-Ende
+		doReturn(mockDI).when(spySpielfeld).createDirectionIterator();
+		doReturn(mockLIHorizontal).when(spySpielfeld).createLineIterator(new Pos(0,0), new Pos(0,1));
+		doReturn(mockLIDiagonal).when(spySpielfeld).createLineIterator(new Pos(0,0), new Pos(1,1));
+		// Partielles Mocking-Ende
 	}
 
 	@Test
 	public void testEsGibtEinenWegVonPosZuFarbeVertikal() {
-		spielfeld = SpielfeldFactory.getSpielfeld4x4ForEsGibtEinenWegVonPosZuFarbeVertikal();
-		Assert.assertTrue(spielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(3,0), Colors.BLACK));
-		Assert.assertFalse(spielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(1,1), Colors.BLACK));
+		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld4x4ForEsGibtEinenWegVonPosZuFarbeVertikal());
+		//b  o  o  o
+		//w  o  o  o
+		//w  o  o  o
+		//o  o  o  o
+		mockingFuerEsGibtEinenOderKeinenVertikal(spySpielfeld);
+		Assert.assertTrue(spySpielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(3,0), Colors.BLACK));
+	}
+
+	@Test
+	public void testEsGibtKeinenWegVonPosZuFarbeVertikal() {
+		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld4x4ForEsGibtKeinenWegVonPosZuFarbeVertikal());
+		//b  o  o  o
+		//w  o  o  o
+		//o  o  o  o
+		//o  o  o  o
+		mockingFuerEsGibtEinenOderKeinenVertikal(spySpielfeld);
+		Assert.assertFalse(spySpielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(3,0), Colors.BLACK));
+	}
+
+
+	private void mockingFuerEsGibtEinenOderKeinenVertikal(Spielfeld spySpielfeld) {
+		// mocking - Iteratoren vollständig-Beginn
+		DirectionIterator mockDI = mock(DirectionIterator.class);
+		when(mockDI.hasNext()).thenReturn( true, true, false);
+		when(mockDI.next()).thenReturn( new Pos(-1,0), new Pos(0,1)); // prüft ausschließlich vertikal (nach oben) und horizontal (nach rechts)
+		
+		LineIterator mockLIVertikal = mock(LineIterator.class);
+		when(mockLIVertikal.hasNext()).thenReturn( true, true, true, true, false);
+		when(mockLIVertikal.next()).thenReturn( new Pos(3,0), new Pos(2,0), new Pos(1,0), new Pos(0,0));
+		
+		LineIterator mockLIHorizontal = mock(LineIterator.class);
+		when(mockLIHorizontal.hasNext()).thenReturn( true, true, true, true, false);
+		when(mockLIHorizontal.next()).thenReturn( new Pos(3,0), new Pos(3,1), new Pos(3,2), new Pos(3,3));
+		// mocking - Iteratoren vollständig-Ende
+		
+		// mocking - Spielfeld partiell/Teil 2-Beginn
+		doReturn(mockDI).when(spySpielfeld).createDirectionIterator();
+		doReturn(mockLIVertikal).when(spySpielfeld).createLineIterator(new Pos(3,0), new Pos(-1,0));
+		doReturn(mockLIHorizontal).when(spySpielfeld).createLineIterator(new Pos(3,0), new Pos(0,1));
+		// mocking - Spielfeld partiell/Teil 2-Ende
 	}
 	
 	@Test
 	public void testEsGibtEinenWegVonPosZuFarbeDiagonal() {
-		spielfeld = SpielfeldFactory.getSpielfeld4x4ForEsGibtEinenWegVonPosZuFarbeDiagonal();
-		Assert.assertTrue(spielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(0,3), Colors.BLACK));
-		Assert.assertFalse(spielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(1,1), Colors.BLACK));
+		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld4x4ForEsGibtEinenWegVonPosZuFarbeDiagonal());
+		mockingFuerEsGibtEinenOderKeinenDiagonal(spySpielfeld);
+		Assert.assertTrue(spySpielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(0,3), Colors.BLACK));
+		//Assert.assertFalse(spielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(1,1), Colors.BLACK));
 	}
 	
+	@Test
+	public void testEsGibtKeinenWegVonPosZuFarbeDiagonal() {
+		Spielfeld spySpielfeld = spy(SpielfeldFactory.getSpielfeld4x4ForEsGibtKeinenWegVonPosZuFarbeDiagonal());
+		mockingFuerEsGibtEinenOderKeinenDiagonal(spySpielfeld);
+		Assert.assertFalse(spySpielfeld.esGibtEinenWegVonPosZuFarbe(new Pos(1,1), Colors.BLACK));
+	}
+
+	
+	private void mockingFuerEsGibtEinenOderKeinenDiagonal(Spielfeld spySpielfeld) {
+		// mocking - Iteratoren vollständig-Beginn
+		DirectionIterator mockDI = mock(DirectionIterator.class);
+		when(mockDI.hasNext()).thenReturn( true, false);
+		when(mockDI.next()).thenReturn( new Pos(1,-1)); // prüft ausschließlich diagonal rechts oben nach links unten
+		
+		LineIterator mockLIDiagonal = mock(LineIterator.class);
+		when(mockLIDiagonal.hasNext()).thenReturn( true, true, true, true, false);
+		when(mockLIDiagonal.next()).thenReturn( new Pos(0,3), new Pos(1,2), new Pos(2,1), new Pos(3,0));
+		// mocking - Iteratoren vollständig-Ende
+		
+		// mocking - Spielfeld partiell/Teil 2-Beginn
+		doReturn(mockDI).when(spySpielfeld).createDirectionIterator();
+		doReturn(mockLIDiagonal).when(spySpielfeld).createLineIterator(new Pos(0,3), new Pos(1,-1));
+		// mocking - Spielfeld partiell/Teil 2-Ende
+	}
+	
+	@Test
+	public void testSetzeSpielstein()
+	{
+		Spielfeld spielfeld = SpielfeldFactory.getSpielfeld4x4ForSetzeSpielstein();
+		// o  w  b  o 
+		// o  o  b  b
+		// o  w  o  b
+		// o  o  o  w
+		spielfeld.setzeSpielstein(Colors.WHITE,new Pos(0,3));
+		Assert.assertEquals(8, spielfeld.anzahl(Colors.WHITE));
+		Assert.assertEquals(0, spielfeld.anzahl(Colors.BLACK));
+	}
 
 }
