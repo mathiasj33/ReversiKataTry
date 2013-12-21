5c5a09f408425333d3ef619e0984b2dca972a850
RobertWeidinger <robert@familie.weidinger-rr.de>
2013-12-21 17:29:27 +0100

    Fehler in DirectionsIterator korrigiert.

--- src/de/rmrw/ReversiKata/code/DirectionIterator.java
+++ src/de/rmrw/ReversiKata/code/DirectionIterator.java
@@ -7,7 +7,7 @@ public class DirectionIterator extends ReversiIterator {
 				new Pos(0, 1),new Pos(0,-1),new Pos(1, 0),new Pos(- 1, 0),new Pos(1, 1),new Pos(1,- 1),new Pos( - 1, 1),new Pos(- 1, - 1)
 		};
 		for(int i = 0; i < directions.length; i++) {
-			if(s.contains(directions[i])) {
+			if(s.contains(p.add(directions[i]))) {
 				positions.add(directions[i]);
 			}
 		}
